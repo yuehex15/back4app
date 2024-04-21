f808c12be314b0444d7240bf2258985c1532047b
yuehex15 <160401007+yuehex15@users.noreply.github.com>
2024-04-21 21:42:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const FILE_PATH = process.env.FILE_PATH || './temp'; // 运行文件夹，节点文件存放目录
 const projectPageURL = process.env.URL || '';        // 填写项目域名可开启自动访问保活，非标端口的前缀是http://
 const intervalInseconds = process.env.TIME || 120;   // 自动访问间隔时间（120秒）
-const UUID = process.env.UUID || '9d18402b-fc6a-4fba-8806-7ecfcb4594de';
-const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.tz.x10.mx';      // 哪吒3个变量不全不运行
-const NEZHA_PORT = process.env.NEZHA_PORT || '443';              // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
-const NEZHA_KEY = process.env.NEZHA_KEY || 'pZkyHae42dCzeABa8V';                   // 哪吒客户端密钥
+const UUID = process.env.UUID || 'bf4d8fc5-30b1-4d12-b5db-e97c7d1cc8fa';
+const NEZHA_SERVER = process.env.NEZHA_SERVER || '';      // 哪吒3个变量不全不运行
+const NEZHA_PORT = process.env.NEZHA_PORT || '';              // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
+const NEZHA_KEY = process.env.NEZHA_KEY || '';                   // 哪吒客户端密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';              // 固定隧道域名，留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || '';                 // 固定隧道json或token，留空即启用临时隧道
 const CFIP = process.env.CFIP || 'government.se';             // 优选域名或优选ip
 const CFPORT = process.env.CFPORT || 443;                    // 节点端口
-const NAME = process.env.NAME || 'CodeSandbox';                     // 节点名称
+const NAME = process.env.NAME || 'back4app';                     // 节点名称
 const ARGO_PORT = process.env.ARGO_PORT || 8080;           // Argo端口，使用固定隧道token需和cf后台设置的端口对应
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000; // 节点订阅端口，若无法订阅请手动改为分配的端口
 
